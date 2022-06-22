4f434538f8197427f66d66e16b4de644042d06e8
richard-duong <richtvduong@gmail.com>
2022-06-21 20:23:16 -0500

    created atm.cpp and added prompt for user

---
+++ src/atm.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int input;
+  cout << "1. Deposit" << endl;
+  cout << "2. Withdraw" << endl;
+  cout << "3. Check Balance" << endl;
+  cin >> input;
+
+}
