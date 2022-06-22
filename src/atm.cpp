ec04512f987397c681d5f0ec772e7dfc854f5fb1
kennytruong001 <67294415+kennytruong001@users.noreply.github.com>
2022-06-21 19:00:53 -0700

    created a basic atm interface system

--- src/atm.cpp
+++ src/atm.cpp
@@ -3,9 +3,45 @@ using namespace std;
 
 int main() {
   int input;
-  cout << "1. Deposit" << endl;
-  cout << "2. Withdraw" << endl;
-  cout << "3. Check Balance" << endl;
-  cin >> input;
+  int bal = 0;
+  bool active = true;
+  int diff;
+
+  while (active){
+    cout << "1. Deposit" << endl;
+    cout << "2. Withdraw" << endl;
+    cout << "3. Check Balance" << endl;
+    cout << "4. Exit" << endl;
+    cin >> input;
+
+    if (input == 1){
+      cout << "How much would you like to deposit?: ";
+      cin >> diff;
+      bal += diff;
+    }
+    else if (input == 2){
+      cout << "How much would you like to withdraw?: ";
+      cin >> diff;
+      bal -= diff;
+    }
+
+    else if (input == 3){
+      cout << "Current balance: " << bal << endl; 
+    }
+
+    else if (input == 4){
+      cout << "Thank you, have a nice day!" << endl; 
+      active = false;
+    }
+
+    else{
+      cout << "Invalid input, try again" << endl;
+      
+      cin.clear();
+      cin.ignore();
+    }
+      
+
+  }
 
 }
